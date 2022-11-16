c368f498cf61c57cf3ff73c36763481d129bf152
evasco23 <108623052+evasco23@users.noreply.github.com>
2022-11-15 20:54:21 -0500

    Inserción del Logo

--- front_deve/src/App.js
+++ front_deve/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logoa.png';
 import './App.css';
 
 import CompShowProducts from './Product/ShowAutos';
@@ -10,20 +10,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          
-        </a>
-
-
         
       </header>
       <BrowserRouter>
